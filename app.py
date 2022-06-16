f127d5a4c0e950b10a56938e5b49e03e284a53e7
juanmanueldupi <juanmadupi@gmail.com>
2022-06-16 11:52:43 +0200

    Modificaciones en el programa principal

--- app.py
+++ app.py
@@ -19,28 +19,29 @@ def contacto():
 def sobre_nosotros():
     return render_template("about.html")
 
-@app.route('/movil,methods=["POST"]')
+@app.route('/movil',methods=["POST"])
 def movil():
     key=os.environ["api_key"]
     print ("Formato valido: pefijo+numero; Ej:34123456789")
     phone= []
     phone1= []
     payload = {'api_key':key , 'phone':phone}
+    #payload1 = {'phone':phone1}
     r=requests.get(URL_BASE,params=payload)
-    r2=requests.get(URL_BASE2+phone1)
+    #r2=requests.get(URL_BASE2+payload1)
     if r.status_code == 200:
         doc=r.json()
         print("Telefono",doc.get("phone"))
         print("valido",doc.get("valid"))
         if r2.status_code == 200:
             doc2=r2.json()
-        print("Id:",doc.get("id"))
-        print("Pokemon:",doc.get("name"))
-        print("Sprite:",doc.get("sprites").get("front_default"))
-        print("Tipo:",doc.get("types")[0].get("type").get("name"))
+        print("Id:",doc2.get("id"))
+        print("Pokemon:",doc2.get("name"))
+        print("Sprite:",doc2.get("sprites").get("front_default"))
+        print("Tipo:",doc2.get("types")[0].get("type").get("name"))
     else:
         print ("Error, el telefono no es valido")
-        return render_template("index.html, phone=phone, phone1=phone1")
+        return render_template("movil.html")
 
 #port=os.environ["PORT"]
 #app.run("0.0.0.0",int(port),debug=True)
