e53df26e2e4246bca1dd5a7f9ec84f178b6542b0
juanmanueldupi <juanmadupi@gmail.com>
2022-06-17 13:16:03 +0200

    Funcionando con una sola Api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template,redirect, abort
+from flask import Flask, render_template,request,redirect,url_for
 import os
 import json
 
@@ -24,24 +24,28 @@ def movil():
     key=os.environ["api_key"]
     print ("Formato valido: pefijo+numero; Ej:34123456789")
     phone= []
-    phone1= []
-    payload = {'api_key':key , 'phone':phone}
+    telf=request.form.get("telf")
+#    phone1= []
+    payload = {'api_key':key , 'phone':telf}
+    print (payload)
     #payload1 = {'phone':phone1}
     r=requests.get(URL_BASE,params=payload)
+    print (r)
     #r2=requests.get(URL_BASE2+payload1)
     if r.status_code == 200:
         doc=r.json()
-        print("Telefono",doc.get("phone"))
-        print("valido",doc.get("valid"))
-        if r2.status_code == 200:
-            doc2=r2.json()
-        print("Id:",doc2.get("id"))
-        print("Pokemon:",doc2.get("name"))
-        print("Sprite:",doc2.get("sprites").get("front_default"))
-        print("Tipo:",doc2.get("types")[0].get("type").get("name"))
-    else:
-        print ("Error, el telefono no es valido")
-        return render_template("movil.html")
+        dict={"Telefono":doc.get("format").get("local"), "Valido":doc.get("valid")}
+        print ("Esto es el dicionario", dict)
+        phone.append(dict)
+        print ("Esto es la lista",phone)
+        #if r2.status_code == 200:
+        #    doc2=r2.json()
+    #    print("Id:",doc2.get("id"))
+    #    print("Pokemon:",doc2.get("name"))
+    #    print("Sprite:",doc2.get("sprites").get("front_default"))
+    #    print("Tipo:",doc2.get("types")[0].get("type").get("name"))
+        #print ("Error, el telefono no es valido")
+        return render_template("movil.html", phone=phone)
 
 #port=os.environ["PORT"]
 #app.run("0.0.0.0",int(port),debug=True)
